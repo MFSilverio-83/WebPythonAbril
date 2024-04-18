12b0cad9b7e455dde25ddb837d3fbe8fa7fa486f
MFSilverio-83 <mfsilverio@ymail.com>
2024-04-17 21:03:58 -0300

    aula3

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
-app = Flask("ola")
+app = Flask("Ola")
 
-# raiz
-@app.route('/')          # decorator - rotas
-def ola():
-    return "HomePage"
+DATABASE  = "banco.bd"
+SECRET_KEY = "1234"
 
-# nova aba
-@app.route('/aluno')
-def aluno():
-    return render_template("ola.html")
+app.config.from_object(__name__)
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request    
+def teardown_request(f):
+    g.bd.close()
+
+@app.route('/')
+def exibir_posts():
+    
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado  = g.bd.execute(sql)
+    
+    posts = [
+        {
+        "titulo": "Memórias Póstumas de Brás Cubas", 
+         "texto":"Fui descalçar as botas, que estavam apertadas.", 
+         "data_criacao": "1881"
+    },
+        {
+        "titulo": "Teste", 
+         "texto":"Fui descalçar os testes, que estavam testes.", 
+         "data_criacao": "1990"
+    },
+]
+    return render_template("ola.html", post=posts)
